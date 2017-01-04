679d803196dca990bf0eb9a0f357427b0bccf012
mostafa_anter <ga30000@gmail.com>
2017-01-04 21:12:19 +0200

    multi drawables

---
+++ app/src/main/java/com/tab3e/activity/SplashActivity.java
@@ -0,0 +1,68 @@
+package com.tab3e.activity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.tab3e.R;
+import com.tab3e.R2;
+import com.tab3e.util.Util;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @BindView(R2.id.text1)TextView loadingText;
+    @BindView(R2.id.progressBar)ProgressBar progressBar;
+
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+
+        // change text font
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", loadingText);
+
+        // loading task
+        loadingTask();
+    }
+
+    private void loadingTask() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(progressStatus < 100){
+                    // Update the progress status
+                    progressStatus +=1;
+
+                    // Try to sleep the thread for 20 milliseconds
+                    try{
+                        Thread.sleep(20);
+                    }catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
+
+                    // Update the progress bar
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                            if (progressStatus == 100)
+                                startActivity(new Intent(SplashActivity.this, LoginActivity.class)
+                                        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK));
+
+                        }
+                    });
+                }
+            }
+        }).start(); // Start the operation
+    }
+}
