04acbe19b8b5742b74a33ed28def4dd6b8aec9c6
mostafa_anter <ga30000@gmail.com>
2017-01-05 16:48:24 +0200

    multi drawables

--- app/src/main/java/com/tab3e/activity/RegistrationActivity.java
+++ app/src/main/java/com/tab3e/activity/RegistrationActivity.java
@@ -1,6 +1,5 @@
 package com.tab3e.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
@@ -17,37 +16,42 @@ import com.tab3e.util.Util;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class LoginActivity extends AppCompatActivity implements View.OnFocusChangeListener,
+public class RegistrationActivity extends AppCompatActivity implements View.OnFocusChangeListener,
         View.OnClickListener{
 
     @BindView(R2.id.text1)TextView textView1;
     @BindView(R2.id.text2)TextView textView2;
     @BindView(R2.id.text3)TextView textView3;
-    @BindView(R2.id.text4)TextView textView4;
-    @BindView(R2.id.text5)TextView textView5;
-    @BindView(R2.id.text6)TextView textView6;
+
     @BindView(R2.id.editText1)EditText editText1;
     @BindView(R2.id.editText2)EditText editText2;
-    @BindView(R2.id.checkbox1)CheckBox checkBox1;
+    @BindView(R2.id.editText3)EditText editText3;
+    @BindView(R2.id.editText4)EditText editText4;
+    @BindView(R2.id.editText5)EditText editText5;
+
     @BindView(R2.id.linear1)LinearLayout linear1;
     @BindView(R2.id.linear2)LinearLayout linear2;
+    @BindView(R2.id.linear3)LinearLayout linear3;
+    @BindView(R2.id.linear4)LinearLayout linear4;
+    @BindView(R2.id.linear5)LinearLayout linear5;
+
+    @BindView(R2.id.checkbox1)CheckBox checkBox1;
 
     @BindView(R2.id.card_view1)CardView cardView1;
-    @BindView(R2.id.card_view2)CardView cardView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
         ButterKnife.bind(this);
 
         // change font
         Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView1);
         Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView2);
         Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView3);
-        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView4);
-        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView5);
-        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView6);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", editText3);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", editText4);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", editText5);
         Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", editText1);
         Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", editText2);
         Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", checkBox1);
@@ -55,10 +59,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnFocusChan
         // set focuse
         editText1.setOnFocusChangeListener(this);
         editText2.setOnFocusChangeListener(this);
+        editText3.setOnFocusChangeListener(this);
+        editText4.setOnFocusChangeListener(this);
+        editText5.setOnFocusChangeListener(this);
 
         // on click
         cardView1.setOnClickListener(this);
-        cardView2.setOnClickListener(this);
+
     }
 
     @Override
@@ -72,6 +79,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnFocusChan
                     case R.id.editText2:
                         linear2.setBackgroundResource(R.drawable.border_shape_blue);
                         break;
+                    case R.id.editText3:
+                        linear3.setBackgroundResource(R.drawable.border_shape_blue);
+                        break;
+                    case R.id.editText4:
+                        linear4.setBackgroundResource(R.drawable.border_shape_blue);
+                        break;
+                    case R.id.editText5:
+                        linear5.setBackgroundResource(R.drawable.border_shape_blue);
+                        break;
                 }
             }else {
                 switch (view.getId()){
@@ -80,6 +96,15 @@ public class LoginActivity extends AppCompatActivity implements View.OnFocusChan
                         break;
                     case R.id.editText2:
                         linear2.setBackgroundResource(R.drawable.border_shape_gray);
+                        break;
+                    case R.id.editText3:
+                        linear3.setBackgroundResource(R.drawable.border_shape_gray);
+                        break;
+                    case R.id.editText4:
+                        linear4.setBackgroundResource(R.drawable.border_shape_gray);
+                        break;
+                    case R.id.editText5:
+                        linear5.setBackgroundResource(R.drawable.border_shape_gray);
                         break;
                 }
             }
@@ -90,9 +115,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnFocusChan
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.card_view1:
-                break;
-            case R.id.card_view2:
-                startActivity(new Intent(this, RegistrationActivity.class));
                 break;
         }
     }
