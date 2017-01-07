92319d0f5c28f9aca13f69a8ad996249c45dacd2
mostafa_anter <ga30000@gmail.com>
2017-01-07 16:21:40 +0200

    stable version

--- app/src/main/java/com/tab3e/activity/ResultOfAskAboutStudent.java
+++ app/src/main/java/com/tab3e/activity/ResultOfAskAboutStudent.java
@@ -1,14 +1,17 @@
 package com.tab3e.activity;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.tab3e.R;
 import com.tab3e.R2;
+import com.tab3e.util.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,6 +21,23 @@ public class ResultOfAskAboutStudent extends AboutTab3e
 
     @BindView(R2.id.toolbar)Toolbar toolbar;
     @BindView(R2.id.nav_view)NavigationView navigationView;
+
+    @Nullable @BindView(R2.id.text1)TextView textView1;
+    @Nullable @BindView(R2.id.text2)TextView textView2;
+    @Nullable @BindView(R2.id.text3)TextView textView3;
+    @Nullable @BindView(R2.id.text4)TextView textView4;
+    @Nullable @BindView(R2.id.text5)TextView textView5;
+    @Nullable @BindView(R2.id.text6)TextView textView6;
+    @Nullable @BindView(R2.id.text7)TextView textView7;
+    @Nullable @BindView(R2.id.text8)TextView textView8;
+    @Nullable @BindView(R2.id.text9)TextView textView9;
+    @Nullable @BindView(R2.id.text10)TextView textView10;
+    @Nullable @BindView(R2.id.text11)TextView textView11;
+    @Nullable @BindView(R2.id.text12)TextView textView12;
+    @Nullable @BindView(R2.id.text13)TextView textView13;
+    @Nullable @BindView(R2.id.text14)TextView textView14;
+    @Nullable @BindView(R2.id.text15)TextView textView15;
+    @Nullable @BindView(R2.id.text16)TextView textView16;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +55,9 @@ public class ResultOfAskAboutStudent extends AboutTab3e
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         changeFontOfNavigation();
+
+        // change text font
+        changeFont();
     }
 
     @Override
@@ -45,5 +68,24 @@ public class ResultOfAskAboutStudent extends AboutTab3e
         } else {
             super.onBackPressed();
         }
+    }
+
+    private void changeFont(){
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView1);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView2);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView3);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView4);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView5);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView6);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView7);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView8);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView9);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView10);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView11);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView12);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView13);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView14);
+        //Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView15);
+        //Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView16);
     }
 }
