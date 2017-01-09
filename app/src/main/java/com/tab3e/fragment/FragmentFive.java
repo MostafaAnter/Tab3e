737a84317f25bfde3a26f07edacf4ced9c33716f
mostafa_anter <ga30000@gmail.com>
2017-01-09 19:46:42 +0200

    stable version

---
+++ app/src/main/java/com/tab3e/fragment/FragmentFive.java
@@ -0,0 +1,27 @@
+package com.tab3e.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tab3e.R;
+
+/**
+ * Created by mostafa_anter on 1/8/17.
+ */
+
+public class FragmentFive extends Fragment{
+    public FragmentFive(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+        return view;
+    }
+}
