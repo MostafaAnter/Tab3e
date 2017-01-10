ad970a4cf6f770ca5a22156fb8e18fd911a4b26b
mostafa_anter <ga30000@gmail.com>
2017-01-10 11:46:18 +0200

    stable version

--- app/src/main/java/com/tab3e/activity/StudentDetails.java
+++ app/src/main/java/com/tab3e/activity/StudentDetails.java
@@ -1,14 +1,17 @@
 package com.tab3e.activity;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.tab3e.R;
 import com.tab3e.R2;
+import com.tab3e.util.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,6 +21,16 @@ public class StudentDetails extends AboutTab3e
 
     @BindView(R2.id.toolbar)Toolbar toolbar;
     @BindView(R2.id.nav_view)NavigationView navigationView;
+
+    @Nullable
+    @BindView(R2.id.text1)
+    TextView textView1;
+    @Nullable
+    @BindView(R2.id.text2)
+    TextView textView2;
+    @Nullable
+    @BindView(R2.id.text3)
+    TextView textView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +48,10 @@ public class StudentDetails extends AboutTab3e
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         changeFontOfNavigation();
+
+        //change text font
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView1);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView2);
     }
 
     @Override
