c940bf68f305ab3122e7b518877ace6dd686fa88
mostafa_anter <ga30000@gmail.com>
2017-01-10 14:47:31 +0200

    stable version

--- app/src/main/java/com/tab3e/activity/ContactWithSchool.java
+++ app/src/main/java/com/tab3e/activity/ContactWithSchool.java
@@ -1,14 +1,26 @@
 package com.tab3e.activity;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.tab3e.R;
 import com.tab3e.R2;
+import com.tab3e.adapter.SpinnerCustomAdapter;
+import com.tab3e.model.SpinnerModel;
+import com.tab3e.util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,6 +30,67 @@ public class ContactWithSchool extends AboutTab3e
 
     @BindView(R2.id.toolbar)Toolbar toolbar;
     @BindView(R2.id.nav_view)NavigationView navigationView;
+
+    @Nullable
+    @BindView(R2.id.text1)
+    TextView textView1;
+    @Nullable
+    @BindView(R2.id.text2)
+    TextView textView2;
+    @Nullable
+    @BindView(R2.id.text3)
+    TextView textView3;
+    @Nullable
+    @BindView(R2.id.text4)
+    TextView textView4;
+    @Nullable
+    @BindView(R2.id.text5)
+    TextView textView5;
+    @Nullable
+    @BindView(R2.id.text6)
+    TextView textView6;
+    @Nullable
+    @BindView(R2.id.text7)
+    TextView textView7;
+    @Nullable
+    @BindView(R2.id.text8)
+    TextView textView8;
+    @Nullable
+    @BindView(R2.id.text9)
+    TextView textView9;
+    @Nullable
+    @BindView(R2.id.text10)
+    TextView textView10;
+    @Nullable
+    @BindView(R2.id.text11)
+    TextView textView11;
+    @Nullable
+    @BindView(R2.id.text12)
+    TextView textView12;
+    @Nullable
+    @BindView(R2.id.text13)
+    TextView textView13;
+    @Nullable
+    @BindView(R2.id.text14)
+    TextView textView14;
+    @Nullable
+    @BindView(R2.id.text15)
+    TextView textView15;
+    @Nullable
+    @BindView(R2.id.text16)
+    TextView textView16;
+    @Nullable
+    @BindView(R2.id.text17)
+    TextView textView17;
+    @Nullable
+    @BindView(R2.id.text18)
+    TextView textView18;
+    @Nullable
+    @BindView(R2.id.text19)
+    TextView textView19;
+
+    @Nullable
+    @BindView(R2.id.spinner1)Spinner spinner1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +108,33 @@ public class ContactWithSchool extends AboutTab3e
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         changeFontOfNavigation();
+
+        // set spinner
+        List<SpinnerModel> models = new ArrayList<>();
+        models.add(new SpinnerModel("المدرسة", ""));
+        populateSpinner1(models);
+
+        //change text font
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView1);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView2);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView3);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView4);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView5);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView6);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView7);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView8);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView9);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView10);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView11);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView12);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView13);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView14);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView15);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView16);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView17);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView18);
+        Util.changeViewTypeFace(this, "fonts/DroidKufi-Regular.ttf", textView19);
+
     }
 
     @Override
@@ -45,5 +145,34 @@ public class ContactWithSchool extends AboutTab3e
         } else {
             super.onBackPressed();
         }
+    }
+
+    private void populateSpinner1(List<SpinnerModel> mlist) {
+
+        SpinnerCustomAdapter spinnerArrayAdapter = new SpinnerCustomAdapter(this, R.layout.spinner_item, mlist);
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
+        spinner1.setAdapter(spinnerArrayAdapter);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+                //String selectedItemText = (String) parent.getItemAtPosition(position);
+                SpinnerModel selectedItem = (SpinnerModel) parent.getItemAtPosition(position);
+                if (position > 0) {
+                    // doSome things
+//                    countryId = selectedItem.getId();
+//                    cityId = null;
+//                    getCities(countryId);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
     }
 }
