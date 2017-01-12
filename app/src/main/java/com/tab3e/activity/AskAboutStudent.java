b46169ba10fa6897c90267fcd55d379f784bc7f6
mostafa_anter <ga30000@gmail.com>
2017-01-12 13:49:09 +0200

    stable version

--- app/src/main/java/com/tab3e/activity/AskAboutStudent.java
+++ app/src/main/java/com/tab3e/activity/AskAboutStudent.java
@@ -136,12 +136,14 @@ public class AskAboutStudent extends AboutTab3e
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
+
+        super.onBackPressed();
     }
 
     @Override
